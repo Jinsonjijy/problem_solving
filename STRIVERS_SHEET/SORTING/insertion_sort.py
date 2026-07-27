2dbc5aad45adf0c55fd23c1ddbca46fbcf9080ab
jinson jijy <jinsonjijy50@gmail.com>
2026-07-27 15:06:46 +0530

    done the working of the insertion sort

--- STRIVERS_SHEET/SORTING/insertion_sort.py
+++ STRIVERS_SHEET/SORTING/insertion_sort.py
@@ -6,11 +6,11 @@ lets create a insertion sort algorithm
 def insertion_sort(arr):
     for i in range(1,len(arr)):
         j=i-1
-        while (j<0 and arr[j+1]<arr[j]):
+        while (j>=0 and arr[j+1]<arr[j]):
             temp=arr[j+1]
             arr[j+1]=arr[j]
             arr[j]=temp
-            j=-1
+            j=j-1
 arr=list(map(int,input().split(" ")))
 insertion_sort(arr)
 print(arr)
