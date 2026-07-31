b6ce9f7133372fcc98e9b1aec7f7bf7f306dd935
jinson jijy <jinsonjijy50@gmail.com>
2026-07-31 09:11:48 +0530

    climbing stairs problem is done

---
+++ STRIVERS_SHEET/DYNAMIC PROGRAMMING/climbing_stairs.py
@@ -0,0 +1,42 @@
+"""
+while working on the climbing stairs problem
+70. Climbing Stairs
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+"""
+def climbing_stairs(n,dp):
+    if n==0:
+        return 1
+    if n==1:
+        return 1
+    if dp[n]!=-1:
+        return dp[n]
+    dp[n]=climbing_stairs(n-1,dp)+climbing_stairs(n-2,dp)
+    return dp[n]
+n=int(input(""))
+dp=[-1]*(n+1)
+print(climbing_stairs(n,dp))
