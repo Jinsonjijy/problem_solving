bcd15808db329bbaedab1334046f0efe05540df8
JINSON JIJI <131127230+Jinsonjijy@users.noreply.github.com>
2026-08-09 12:47:56 +0000

    done some new chnages

--- STRIVERS_SHEET/RECURSION/sort_stack.py
+++ STRIVERS_SHEET/RECURSION/sort_stack.py
@@ -13,6 +13,7 @@ Sort a Stack
 10
 
 Problem Statement: You are given a stack of integers. Your task is to sort the stack in descending order using recursion, such that the top of the stack contains the greatest element. You are not allowed to use any loop-based sorting methods (e.g., quicksort, mergesort). You may only use recursive operations and the standard stack operations (push, pop, peek/top, and isEmpty).
+thi
 """
 def insert_stack(stack,x):
     if not stack or stack[-1]<=x:
