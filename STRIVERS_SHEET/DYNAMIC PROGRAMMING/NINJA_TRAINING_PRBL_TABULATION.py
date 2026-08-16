ccde163f89151e74c080606408b14b5a149a193f
jinson jijy <jinsonjijy50@gmail.com>
2026-08-16 18:48:59 +0530

    working on the ninja_training_problem

---
+++ STRIVERS_SHEET/DYNAMIC PROGRAMMING/NINJA_TRAINING_PRBL_TABULATION.py
@@ -0,0 +1,55 @@
+"""
+Ninja's Training
+58 mins read
+The Jat
+Easy
+Updated 1 year ago
+Dynamic  Programming
+Problem Statement
+A ninja has planned a n-day training schedule. Each day he has to perform one of three activities - running, stealth training, or fighting practice. The same activity cannot be done on two consecutive days and the ninja earns a specific number of merit points, based on the activity and the given day.Programming
+
+Given a n x 3-sized matrix, where matrix[i][0], matrix[i][1], and matrix[i][2], represent the merit points associated with running, stealth and fighting practice, on the (i+1)th day respectively. Return the maximum possible merit points that the ninja can earn.
+
+Examples
+plaintext
+Copy
+Example 1:
+
+Input: matrix = [[10, 40, 70], [20, 50, 80], [30, 60, 90]]
+Output: 210
+
+Explanation:
+Day 1: fighting practice = 70
+Day 2: stealth training = 50
+Day 3: fighting practice = 90
+Total = 70 + 50 + 90 = 210
+This gives the optimal points.
+plaintext
+Copy
+Example 2:
+
+Input: matrix = [[70, 40, 10], [180, 20, 5], [200, 60, 30]]
+Output: 290
+
+Explanation:
+Day 1: running = 70
+Day 2: stealth training = 20
+Day 3: running = 200
+Total = 70 + 20 + 200 = 290
+This gives the optimal points.
+over commed the overlapping subproblem
+
+
+"""
+if __name__=="__main__":
+    arr=[]
+    day=int(input("enter the number of days"))
+    for i in range(day):
+        row=list(map(int,input().split(" ")))
+        arr.append(row)
+        row=[]
+        dp=[[0]*4]*len(arr)
+    print(*arr,sep="\n")
+    for day in range(len(arr)):
+        for task in range(3):
+            if 
