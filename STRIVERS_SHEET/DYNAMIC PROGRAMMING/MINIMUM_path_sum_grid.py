89182b9d2250f872c59c7b6f02327efe5347da0b
jinson jijy <jinsonjijy50@gmail.com>
2026-08-19 23:26:03 +0530

    done the working of the minimum path finding problem using minimum path caluculation using the memoization approach by implementing a dp matrix

--- STRIVERS_SHEET/DYNAMIC PROGRAMMING/MINIMUM_path_sum_grid.py
+++ STRIVERS_SHEET/DYNAMIC PROGRAMMING/MINIMUM_path_sum_grid.py
@@ -17,7 +17,24 @@ Input : grid = [[1,2,3],[4,5,6]]
 Output: 12
 Explanation : Minimum sum is achieved via path 1->2->3->6 i.e. 12
 """
+def min_path_finding(arr):
+    dp=[[-1]*len(arr[0]) for _ in range(len(arr))]
+    def backtracking(i,j):
+        if i==0 and j==0:
+            return arr[0][0]
+        if i<0 or j<0:
+            return float("inf")
+        if dp[i][j]!=-1:
+            return dp[i][j]
+        up=arr[i][j]+backtracking(i-1,j)
+        left=arr[i][j]+backtracking(i,j-1)
+        dp[i][j] = min(up,left)
+        return dp[i][j]
+    return backtracking(len(arr)-1,len(arr[0])-1)
 def min_path(arr):
+    """
+    this approach is a greedy approach and it is implemented by myself
+    """
     sum1=0
     def backtracking(ind,next,sum1):
         if ind==0 and next ==0:
@@ -44,4 +61,4 @@ if __name__=="__main__":
         arr.append(row)
         row=[]
     print(*arr,sep="\n")
-    print(min_path(arr))
+    print(min_path_finding(arr))
