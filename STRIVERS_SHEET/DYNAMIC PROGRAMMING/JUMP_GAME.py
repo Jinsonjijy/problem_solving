3c73e83826e9b4fac15b81006a419901e657c5a2
jinson jijy <jinsonjijy50@gmail.com>
2026-08-28 22:09:35 +0530

    working on the jump game problem and found a dp solution but it is not optimal

---
+++ STRIVERS_SHEET/DYNAMIC PROGRAMMING/JUMP_GAME.py
@@ -0,0 +1,50 @@
+"""
+55. Jump Game
+Attempted
+Medium
+Topics
+premium lock icon
+Companies
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+
+"""
+
+
+
+def finding_path(arr):
+    n=len(arr)
+    dp=[-1]*n
+    def backtracking(ind):
+        if ind>=n-1:
+            return True
+        if arr[ind]==0:
+            return False
+        if dp[ind]!=-1:
+            return dp[ind]
+        for i in range(1,arr[ind]+1):
+            next_ind=ind+i
+            if backtracking(next_ind):
+                dp[ind]=True
+                return True
+
+        dp[ind]=False
+        return False
+if __name__=="__main__":
+    arr=map(int,input().split(" "))
+    finding_path(arr)
